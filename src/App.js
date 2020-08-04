0cdf6b1c31f682b0710a9e0f1935e03d427b256b
Sriram Vasan <ramsrioct98@gmail.com>
2020-08-04 22:57:14 +0530

    v0.0.2-componentsP1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponents'
 import './App.css';
 
 function App() {
-    return ( < div className = "App" >
-        <
-        Navbar dark color = "primary" >
-        <
-        div className = "container" >
-        <
-        NavbarBrand href = "/" > Ristorante Con Fusion < /NavbarBrand>   < /
-        div > <
-        /Navbar>   < /
-        div >
+    return ( <div className = "App" >
+            <Navbar dark color = "primary" >
+                <div className = "container" >
+                    <NavbarBrand href = "/" > Ristorante Con Fusion </NavbarBrand>   
+                </div > 
+            </Navbar> 
+            <Menu/>    
+        </div >
     );
 }
 
